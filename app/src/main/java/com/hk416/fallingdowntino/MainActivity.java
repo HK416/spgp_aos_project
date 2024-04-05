f74e145f738b5fec56db471099bed9c875cc90e4
HK416 <powerspirit127@gmail.com>
2024-04-05 10:10:58 +0900

    안드로이드 프로젝트 초기화.

---
+++ app/src/main/java/com/hk416/fallingdowntino/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hk416.fallingdowntino;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
