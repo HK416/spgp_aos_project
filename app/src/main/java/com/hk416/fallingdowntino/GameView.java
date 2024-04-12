df557b59d65809dc3732a7b0f17b685f0a840744
HK416 <powerspirit127@gmail.com>
2024-04-12 11:31:43 +0900

    GameView와 MainActivity를 연결함.

--- app/src/main/java/com/hk416/fallingdowntino/GameView.java
+++ app/src/main/java/com/hk416/fallingdowntino/GameView.java
@@ -1,12 +1,19 @@
 package com.hk416.fallingdowntino;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 
 
 public class GameView extends View {
     public GameView(Context context) {
         super(context);
+    }
+
+    @Override
+    protected void onDraw(@NonNull Canvas canvas) {
+        super.onDraw(canvas);
     }
 }
--- app/src/main/java/com/hk416/fallingdowntino/MainActivity.java
+++ app/src/main/java/com/hk416/fallingdowntino/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private GameView gameView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        gameView = new GameView(this);
+        setContentView(gameView);
     }
 }
