7778525f9dca09d66a0e87f65b7fd7fae379c779
HK416 <powerspirit127@gmail.com>
2024-04-12 11:25:24 +0900

    GameView 클래스 뼈대 추가.

---
+++ app/src/main/java/com/hk416/fallingdowntino/GameView.java
@@ -0,0 +1,12 @@
+package com.hk416.fallingdowntino;
+
+import android.content.Context;
+import android.view.View;
+
+
+
+public class GameView extends View {
+    public GameView(Context context) {
+        super(context);
+    }
+}
