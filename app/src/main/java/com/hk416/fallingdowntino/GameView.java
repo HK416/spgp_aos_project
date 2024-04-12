1c24d1091dc3088df9ad5d53ff8e63c585bcc55e
HK416 <powerspirit127@gmail.com>
2024-04-12 11:41:20 +0900

    게임 루프 뼈대 작성.
    - GameView에 Choreographer.FrameCallback을 구현함.
    - Choreographer의 postFrameCallback을 호출하여 매 프레임마다 게임 루프를 실행.

--- app/src/main/java/com/hk416/fallingdowntino/GameView.java
+++ app/src/main/java/com/hk416/fallingdowntino/GameView.java
@@ -2,18 +2,34 @@ package com.hk416.fallingdowntino;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.util.Log;
+import android.view.Choreographer;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 
 
-public class GameView extends View {
+public class GameView extends View implements Choreographer.FrameCallback {
+    public static final String TAG = GameView.class.getSimpleName();
+
     public GameView(Context context) {
         super(context);
+
+        run();
     }
 
     @Override
     protected void onDraw(@NonNull Canvas canvas) {
         super.onDraw(canvas);
+    }
+
+    private void run() {
+        Choreographer.getInstance().postFrameCallback(this);
+    }
+
+    @Override
+    public void doFrame(long frameTimeNanos) {
+        Log.d(TAG, "doFrame!");
+        run();
     }
 }
