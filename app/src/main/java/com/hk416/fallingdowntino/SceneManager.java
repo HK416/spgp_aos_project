91e6ee092246073275ab678b840f62a9372cb49e
HK416 <powerspirit127@gmail.com>
2024-04-15 10:28:05 +0900

    SceneManager 클래스 수정.
    - static 변수들을 추가함.
    - 맴버 변수들을 추가함.

--- app/src/main/java/com/hk416/fallingdowntino/SceneManager.java
+++ app/src/main/java/com/hk416/fallingdowntino/SceneManager.java
@@ -1,5 +1,22 @@
 package com.hk416.fallingdowntino;
 
+import java.util.Stack;
+
 public final class SceneManager {
-    
+    public static final String TAG = SceneManager.class.getSimpleName();
+
+    private static final SceneManager instance = new SceneManager();
+
+    private static final int CMD_NONE = 0;
+    public static final int CMD_PUSH = 1;
+    public static final int CMD_POP = 2;
+    public static final int CMD_CHANGE = 3;
+
+    private Stack<IGameScene> sceneStack = new Stack<>();
+    private IGameScene nextScene = null;
+    private int commands = CMD_NONE;
+
+    public static SceneManager getInstance() {
+        return instance;
+    }
 }
