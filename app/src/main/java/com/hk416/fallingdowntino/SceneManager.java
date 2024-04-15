0878d95f68d80e0f47f2dc4faac122dec169de49
HK416 <powerspirit127@gmail.com>
2024-04-15 11:03:57 +0900

    SceneManager에 다음 장면 설정을 수행하는 메소드를 추가함.
    - 다음 장면을 설정하는 명령어 메소드임을 알 수 있도록 함수 이름을 변경함.
    - 각 명령어에 따라 다음 장면 설정을 수행하는 메소드들을 추가함.
    - 각 명령어에 따라 분기하는 명령어 수행 메소드를 추가함.

--- app/src/main/java/com/hk416/fallingdowntino/SceneManager.java
+++ app/src/main/java/com/hk416/fallingdowntino/SceneManager.java
@@ -1,5 +1,7 @@
 package com.hk416.fallingdowntino;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import java.util.Stack;
@@ -22,17 +24,80 @@ public final class SceneManager {
         return instance;
     }
 
-    public void changeScene(@NonNull IGameScene scene) {
+    public void processCommands() {
+        switch (commands) {
+            case CMD_NONE: /* empty */
+                break;
+            case CMD_PUSH:
+                processPushScene();
+                break;
+            case CMD_POP:
+                processPopScene();
+                break;
+            case CMD_CHANGE:
+                processChangeScene();
+                break;
+            default:
+                Log.w(TAG, "::processCommands >> 유효하지 않은 명령어 입니다.");
+                break;
+        }
+        cmdReset();
+    }
+
+    private IGameScene getCurrentScene() {
+        return sceneStack.isEmpty() ? null : sceneStack.peek();
+    }
+
+    private void processChangeScene() {
+        IGameScene currentScene = getCurrentScene();
+        if (currentScene != null) {
+            // TODO: 현재 장면에서 빠져나옵니다.
+            sceneStack.pop();
+        }
+
+        // TODO: 다음 장면에 진입합니다.
+        sceneStack.push(nextScene);
+    }
+
+    private void processPushScene() {
+        IGameScene currentScene = getCurrentScene();
+        if (currentScene != null) {
+            // TODO: 현재 장면을 정지시킵니다.
+        }
+
+        // TODO: 다음 장면에 진입합니다.
+        sceneStack.push(nextScene);
+    }
+
+    private void processPopScene() {
+        IGameScene currentScene = getCurrentScene();
+        if (currentScene != null) {
+            // TODO: 현재 장면에서 빠져나옵니다.
+            sceneStack.pop();
+        }
+
+        currentScene = getCurrentScene();
+        if (currentScene != null) {
+            // TODO: 이전 장면을 재개합니다.
+        }
+    }
+
+    public void cmdReset() {
+        nextScene = null;
+        commands = CMD_NONE;
+    }
+
+    public void cmdChangeScene(@NonNull IGameScene scene) {
         nextScene = scene;
         commands = CMD_CHANGE;
     }
 
-    public void pushScene(@NonNull IGameScene scene) {
+    public void cmdPushScene(@NonNull IGameScene scene) {
         nextScene = scene;
         commands = CMD_PUSH;
     }
 
-    public void popScene() {
+    public void cmdPopScene() {
         nextScene = null;
         commands = CMD_POP;
     }
