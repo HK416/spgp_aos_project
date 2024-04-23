4e6684f73179e5c15ebf5d0cb3ad36b3b541f385
HK416 <powerspirit127@gmail.com>
2024-04-23 11:55:13 +0900

    Tino 기본 상태 이미지를 프로젝트에 추가, 기본 상태 이미지를 화면에 출력함.

--- app/src/main/java/com/hk416/fallingdowntino/object/Tino.java
+++ app/src/main/java/com/hk416/fallingdowntino/object/Tino.java
@@ -1,14 +1,18 @@
 package com.hk416.fallingdowntino.object;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PointF;
+import android.graphics.RectF;
 
 import androidx.annotation.NonNull;
 
 import com.hk416.fallingdowntino.BuildConfig;
+import com.hk416.fallingdowntino.R;
 import com.hk416.framework.object.GameObject;
 import com.hk416.framework.render.DrawPipeline;
+import com.hk416.framework.texture.BitmapPool;
 import com.hk416.framework.transform.Vector;
 
 public class Tino extends GameObject {
@@ -16,16 +20,32 @@ public class Tino extends GameObject {
     private static final float WIDTH = 2.0f;
     private static final float HEIGHT = 2.0f;
     private static final float X_POS = 0.0f;
-    private static final float Y_POS = 12.0f;
+    private static final float Y_POS = 12.5f;
 
-    private final Paint debugColor;
+    private static Paint debugColor = null;
+
+    private Bitmap bitmap = null;
+    private RectF dstRect = new RectF();
 
     public Tino() {
         super(X_POS, Y_POS, WIDTH, HEIGHT);
-        debugColor = new Paint();
-        debugColor.setStrokeWidth(5.0f);
-        debugColor.setStyle(Paint.Style.STROKE);
-        debugColor.setARGB(255, 204, 51, 51);
+        createDebugPaint();
+        createBitmap();
+    }
+
+    private void createDebugPaint() {
+        if (BuildConfig.DEBUG && debugColor == null) {
+            debugColor = new Paint();
+            debugColor.setStrokeWidth(5.0f);
+            debugColor.setStyle(Paint.Style.STROKE);
+            debugColor.setARGB(255, 204, 51, 51);
+        }
+    }
+
+    private void createBitmap() {
+        if (bitmap == null) {
+            bitmap = BitmapPool.getInstance().get(R.mipmap.tino_default_0);
+        }
     }
 
     @Override
@@ -41,6 +61,12 @@ public class Tino extends GameObject {
         PointF bottomLeft = pipeline.toScreenCoord(min);
 
         if (topRight != null && bottomLeft != null) {
+            dstRect.top = topRight.y;
+            dstRect.left = bottomLeft.x;
+            dstRect.bottom = bottomLeft.y;
+            dstRect.right = topRight.x;
+            canvas.drawBitmap(bitmap, null, dstRect, null);
+
             if (BuildConfig.DEBUG) {
                 canvas.drawRect(
                         bottomLeft.x,
