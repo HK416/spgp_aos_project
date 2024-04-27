7c2cd8a9f93e39f9eb79ab431dbccb40b6f04a33
HK416 <powerspirit127@gmail.com>
2024-04-27 17:04:26 +0900

    SpriteAnimeObject 클래스에 메소드 추가 및 Tino 클래스 수정
    - SpriteAnimeObject 클래스의 onUpdate 메소드에서 현재 Bitmap 이미지의 인덱스를 계산함.
    - SpriteAnimeObject 클래스의 onDraw 메소드에서 현재 Bitmap 이미지를 화면에 그림.
    - Tino 클래스가 SpriteAnimeObject 클래스를 상속받도록 수정.

--- app/src/main/java/com/hk416/fallingdowntino/object/Tino.java
+++ app/src/main/java/com/hk416/fallingdowntino/object/Tino.java
@@ -2,24 +2,28 @@ package com.hk416.fallingdowntino.object;
 
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import com.hk416.fallingdowntino.BuildConfig;
 import com.hk416.fallingdowntino.R;
-import com.hk416.framework.object.SpriteObject;
+import com.hk416.framework.object.SpriteAnimeObject;
 
-public class Tino extends SpriteObject {
+public class Tino extends SpriteAnimeObject {
     private static final String TAG = Tino.class.getSimpleName();
     private static final float WIDTH = 2.0f;
     private static final float HEIGHT = 2.0f;
     private static final float X_POS = 0.0f;
     private static final float Y_POS = 12.5f;
+    private static final int[] BITMAP_RES_IDS = new int[] {
+            R.mipmap.tino_default_0, R.mipmap.tino_default_1, R.mipmap.tino_default_2
+    };
 
     private static Paint debugColor = null;
 
     public Tino() {
-        super(R.mipmap.tino_default_0, X_POS, Y_POS, WIDTH, HEIGHT);
+        super(BITMAP_RES_IDS, X_POS, Y_POS, WIDTH, HEIGHT, 0.5f);
         createDebugPaint();
     }
 
@@ -30,6 +34,11 @@ public class Tino extends SpriteObject {
             debugColor.setStyle(Paint.Style.STROKE);
             debugColor.setARGB(255, 204, 51, 51);
         }
+    }
+
+    @Override
+    public void onUpdate(float elapsedTimeSec) {
+        super.onUpdate(elapsedTimeSec);
     }
 
     @Override
--- app/src/main/java/com/hk416/framework/object/SpriteAnimeObject.java
+++ app/src/main/java/com/hk416/framework/object/SpriteAnimeObject.java
@@ -1,5 +1,121 @@
 package com.hk416.framework.object;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.PointF;
+import android.graphics.RectF;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.hk416.framework.render.DrawPipeline;
+import com.hk416.framework.texture.BitmapPool;
+import com.hk416.framework.transform.Vector;
+
 public class SpriteAnimeObject extends GameObject {
-    
+    private static final String TAG = SpriteAnimeObject.class.getSimpleName();
+
+    protected final Vector size;
+    protected float animationSpeed;
+    protected float animationTimer = 0.0f;
+
+    protected final Bitmap[] bitmaps;
+    protected int currBitmapIndex = 0;
+
+    protected final RectF drawScreenArea = new RectF();
+
+    public SpriteAnimeObject(
+            int[] bitmapResIds,
+            float width,
+            float height,
+            float animationSpeed
+    ) {
+        super();
+        if (bitmapResIds == null || bitmapResIds.length == 0) {
+            throw new RuntimeException("::Constructor >> bitmapResIds는 적어도 1개 이상 있어야 합니다.");
+        }
+
+        this.size = new Vector(width, height, 0.0f);
+        this.animationSpeed = animationSpeed;
+        this.bitmaps = new Bitmap[bitmapResIds.length];
+        for (int i = 0; i < bitmapResIds.length; i++) {
+            this.bitmaps[i] = BitmapPool.getInstance().get(bitmapResIds[i]);
+        }
+    }
+
+    public SpriteAnimeObject(
+            int[] bitmapResIds,
+            float x,
+            float y,
+            float width,
+            float height,
+            float animationSpeed
+    ) {
+        super(x, y);
+        if (bitmapResIds == null || bitmapResIds.length == 0) {
+            throw new RuntimeException("::Constructor >> bitmapResIds는 적어도 1개 이상 있어야 합니다.");
+        }
+
+        this.size = new Vector(width, height, 0.0f);
+        this.animationSpeed = animationSpeed;
+        this.bitmaps = new Bitmap[bitmapResIds.length];
+        for (int i = 0; i < bitmapResIds.length; i++) {
+            this.bitmaps[i] = BitmapPool.getInstance().get(bitmapResIds[i]);
+        }
+    }
+
+    @NonNull
+    public Vector getSize() {
+        return size;
+    }
+
+    public void setSize(float width, float height) {
+        size.x = width;
+        size.y = height;
+    }
+
+    public float getAnimationSpeed() {
+        return animationSpeed;
+    }
+
+    public void setAnimationSpeed(float animationSpeed) {
+        this.animationSpeed = animationSpeed;
+    }
+
+    @Override
+    public void onUpdate(float elapsedTimeSec) {
+        final float EPSILON = 1.401298E-45f;
+        if (Math.abs(animationSpeed) <= EPSILON) {
+            return; // animationSpeed가 0인 경우 onUpdate함수를 실행하지 않는다. (0 나누기 오류 방지)
+        }
+
+        boolean reverse = animationSpeed <= 0.0f;
+        float animationSpeedAbs = Math.abs(animationSpeed);
+        float framePerSeconds = (float)bitmaps.length / animationSpeedAbs;
+        animationTimer = (animationTimer + elapsedTimeSec) % animationSpeedAbs;
+        int currFrame = (int)(animationTimer * framePerSeconds);
+        if (!reverse) {
+            currBitmapIndex = currFrame;
+        } else {
+            currBitmapIndex = (bitmaps.length - 1) - currFrame;
+        }
+    }
+
+    @Override
+    public void onDraw(@NonNull Canvas canvas) {
+        Vector minimum = getPosition().postSub(getSize().postMul(0.5f));
+        Vector maximum = getPosition().postAdd(getSize().postMul(0.5f));
+
+        PointF minPos = DrawPipeline.getInstance().toScreenCoord(minimum);
+        PointF maxPos = DrawPipeline.getInstance().toScreenCoord(maximum);
+
+        if (minPos != null && maxPos != null) {
+            drawScreenArea.top = maxPos.y;
+            drawScreenArea.left = minPos.x;
+            drawScreenArea.bottom = minPos.y;
+            drawScreenArea.right = maxPos.x;
+            canvas.drawBitmap(bitmaps[currBitmapIndex], null, drawScreenArea, null);
+        }
+    }
 }
