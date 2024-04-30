ead5a1f35eb9de450d10c61ef4a8567736c80738
HK416 <powerspirit127@gmail.com>
2024-04-30 21:44:43 +0900

    ScoreUi 클래스 뼈대 생성 및 추가

---
+++ app/src/main/java/com/hk416/fallingdowntino/object/ScoreUi.java
@@ -0,0 +1,9 @@
+package com.hk416.fallingdowntino.object;
+
+import com.hk416.framework.object.UiTextObject;
+
+public class ScoreUi extends UiTextObject {
+    public ScoreUi() {
+        super(null);
+    }
+}
