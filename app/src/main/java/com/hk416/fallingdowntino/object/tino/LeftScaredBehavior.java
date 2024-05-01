a2c412b39639ed240e7258059b20af7b5324aa8a
HK416 <powerspirit127@gmail.com>
2024-05-01 10:28:09 +0900

    LeftScaredBehavior 완성

--- app/src/main/java/com/hk416/fallingdowntino/object/tino/LeftScaredBehavior.java
+++ app/src/main/java/com/hk416/fallingdowntino/object/tino/LeftScaredBehavior.java
@@ -1,10 +1,17 @@
 package com.hk416.fallingdowntino.object.tino;
 
+import android.graphics.Canvas;
+
 import androidx.annotation.NonNull;
 
+import com.hk416.fallingdowntino.BuildConfig;
 import com.hk416.fallingdowntino.R;
 import com.hk416.fallingdowntino.object.Player;
 import com.hk416.framework.object.SpriteAnimeObject;
+import com.hk416.framework.render.DrawPipeline;
+import com.hk416.framework.render.GameCamera;
+import com.hk416.framework.transform.Projection;
+import com.hk416.framework.transform.Vector;
 
 public class LeftScaredBehavior extends SpriteAnimeObject {
     private static final float ANIMATION_SPEED = 0.6f;
@@ -25,5 +32,38 @@ public class LeftScaredBehavior extends SpriteAnimeObject {
                 false
         );
         this.player = player;
+    }
+
+    private void updatePlayerPosition(float elapsedTimeSec) {
+        GameCamera mainCamera = DrawPipeline.getInstance().getMainCamera();
+        Projection projection = mainCamera.getProjection();
+        if (projection == null) {
+            return;
+        }
+
+        Vector newPosition = player.getPosition();
+        newPosition.x -= Player.SPEED * elapsedTimeSec;
+
+        float left = newPosition.x - 0.5f * Tino.WIDTH;
+        if (left <= projection.left) {
+            float diff = projection.left - left;
+            newPosition.x = projection.left + Tino.WIDTH + diff;
+            player.turnBehavior();
+        }
+        player.setPosition(newPosition.x, newPosition.y);
+    }
+
+    @Override
+    public void onUpdate(float elapsedTimeSec) {
+        super.onUpdate(elapsedTimeSec);
+        updatePlayerPosition(elapsedTimeSec);
+    }
+
+    @Override
+    public void onDraw(@NonNull Canvas canvas) {
+        super.onDraw(canvas);
+        if (BuildConfig.DEBUG) {
+            canvas.drawRect(drawScreenArea, Tino.debugColor);
+        }
     }
 }
