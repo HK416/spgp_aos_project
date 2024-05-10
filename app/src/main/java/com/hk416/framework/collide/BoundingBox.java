a2b0364aeccdfcd327f111fa0f79060697a09a0c
HK416 <powerspirit127@gmail.com>
2024-05-10 11:47:33 +0900

    IGameCollider 인터페이스 메소드 수정.
    - isCollide 메소드를 대신 intersects 메소드를 추가함.
    - BoundingBox의 IGameCollider 인터페이스 구현 수정.

--- app/src/main/java/com/hk416/framework/collide/BoundingBox.java
+++ app/src/main/java/com/hk416/framework/collide/BoundingBox.java
@@ -1,6 +1,9 @@
 package com.hk416.framework.collide;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.hk416.framework.transform.Vector;
 
 public final class BoundingBox implements IGameCollider<BoundingBox> {
     public float centerX = 0.0f;
@@ -20,7 +23,7 @@ public final class BoundingBox implements IGameCollider<BoundingBox> {
     }
 
     @Override
-    public boolean isCollide(@NonNull BoundingBox other) {
+    public boolean intersects(@NonNull BoundingBox other, @Nullable Vector depth) {
         float leftSideA = centerX - 0.5f * sizeX;
         float rightSideA = centerX + 0.5f * sizeX;
         float bottomSideA = centerY - 0.5f * sizeY;
@@ -29,16 +32,42 @@ public final class BoundingBox implements IGameCollider<BoundingBox> {
         float leftSideB = other.centerX - 0.5f * other.sizeX;
         float rightSideB = other.centerX + 0.5f * other.sizeX;
         float bottomSideB = other.centerY - 0.5f * other.sizeY;
-        float topSideB = other.centerY - 0.5f * other.sizeY;
+        float topSideB = other.centerY + 0.5f * other.sizeY;
 
-        if (rightSideA < leftSideB || leftSideA > rightSideB) {
-            return false;
+        if (leftSideA <= rightSideB) { // other가 +x 방향으로
+            if (bottomSideA <= topSideB) { // other가 +y 방향으로
+                if (depth != null) {
+                    depth.x = rightSideB - leftSideA;
+                    depth.y = topSideB - bottomSideA;
+                    depth.z = 0.0f;
+                }
+                return true;
+            } else if (topSideA >= bottomSideB) { // other가 -y 방향으로
+                if (depth != null) {
+                    depth.x = rightSideB - leftSideA;
+                    depth.y = bottomSideB - topSideA;
+                    depth.z = 0.0f;
+                }
+                return true;
+            }
+        } else if  (rightSideA >= leftSideB) { // other가 -x 방향으로
+            if (bottomSideA <= topSideB) { // other가 +y 방향으로
+                if (depth != null) {
+                    depth.x = leftSideB - rightSideA;
+                    depth.y = topSideB - bottomSideA;
+                    depth.z = 0.0f;
+                }
+                return true;
+            } else if (topSideA >= bottomSideB) { // other가 -y 방향으로
+                if (depth != null) {
+                    depth.x = leftSideB - rightSideA;
+                    depth.y = bottomSideB - topSideA;
+                    depth.z = 0.0f;
+                }
+                return true;
+            }
         }
 
-        if (topSideA < bottomSideB || bottomSideA > topSideB) {
-            return false;
-        }
-
-        return true;
+        return false;
     }
 }
