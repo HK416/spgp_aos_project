bb4519b3c03fa2b9d0ce017969d51ff0ffc4c835
HK416 <powerspirit127@gmail.com>
2024-05-14 13:04:58 +0900

    Tino의 Happy 상태인 LeftHappyBehavior 클래스 생성 및 추가, Happy 상태에서 사용되는 Bitmap 리소스 추가

---
+++ app/src/main/java/com/hk416/fallingdowntino/object/tino/LeftHappyBehavior.java
@@ -0,0 +1,29 @@
+package com.hk416.fallingdowntino.object.tino;
+
+import androidx.annotation.NonNull;
+
+import com.hk416.fallingdowntino.R;
+import com.hk416.fallingdowntino.object.Player;
+import com.hk416.framework.object.SpriteAnimeObject;
+
+public class LeftHappyBehavior extends SpriteAnimeObject {
+    private static final float ANIMATION_SPEED = 0.8f;
+    private static final int[] BITMAP_RES_IDS = new int[] {
+            R.mipmap.tino_happy_0, R.mipmap.tino_happy_1,
+            R.mipmap.tino_happy_2, R.mipmap.tino_happy_1,
+    };
+
+    private final Player player;
+
+    public LeftHappyBehavior(@NonNull Player player) {
+        super(
+                BITMAP_RES_IDS,
+                Tino.WIDTH,
+                Tino.HEIGHT,
+                ANIMATION_SPEED,
+                false,
+                false
+        );
+        this.player = player;
+    }
+}
